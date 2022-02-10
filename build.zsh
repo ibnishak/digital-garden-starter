bb8fc03adc88eb723e8c718ebfeebf8022ed0ba6
Rizwan Ishak <ibnishak@live.com>
2022-02-10 14:20:26 +0530

    Adding build script

---
+++ build.zsh
@@ -0,0 +1,20 @@
+#!/usr/bin/env zsh
+
+
+SOURCE="/path/to/markdown/folder"
+
+# Sync files from source folder to target.
+# This is done so that `.md` files can be converted to `.mdx`
+## Implementation of things like admonitions will be easier in mdx
+echo "Syncing"
+rsync -a ${SOURCE}/ content
+
+echo "Converting to MDX"
+# Convert all `.md` files to `.mdx`
+for f in contents/*.md; do 
+    mv -- "$f" "${f%.md}.mdx"
+done
+
+# build
+echo "Starting build"
+gatsby build
