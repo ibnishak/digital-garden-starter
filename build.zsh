b876e54b6a78feb2c5d13ba6f6eb6fca35f5a276
Rizwan Ishak <ibnishak@live.com>
2022-02-10 14:31:18 +0530

    Adding git commands to build

--- build.zsh
+++ build.zsh
@@ -11,10 +11,15 @@ rsync -a ${SOURCE}/ content
 
 echo "Converting to MDX"
 # Convert all `.md` files to `.mdx`
-for f in contents/*.md; do 
+for f in content/*.md; do 
     mv -- "$f" "${f%.md}.mdx"
 done
 
 # build
 echo "Starting build"
 gatsby build
+
+#Git update
+git add public/*
+git commit -m $(date)
+git subtree push --prefix public origin gh-pages
